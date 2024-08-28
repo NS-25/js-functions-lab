0daf8ea3c55a3d99f4064d6494ca9ca380dfcd22
George-25 <georgemallory26@gmail.com>
2024-08-28 15:21:18 -0400

    exercise-3

--- app.js
+++ app.js
@@ -8,15 +8,15 @@ If they're equal, return either one.
 Exercise 1 has been completed for you:
 */
 
-const maxOfTwoNumbers = (x, y) => {
-  if (x >= y) {
-    return x;
-  } else {
-    return y;
-  }
-}
+// const maxOfTwoNumbers = (x, y) => {
+//   if (x >= y) {
+//     return x;
+//   } else {
+//     return y;
+//   }
+// }
 
-console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+// console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
 ///////////////////////////////////////////////////////////
 /*
 Exercise 2: isAdult()
@@ -28,12 +28,41 @@ Example: isAdult(21) should return 'Adult'.
 
 Complete the exercise in the space below:
 */
-const isAdult = (age) => {
-if (age >= 18){
-  return "Adult";
-} else {
-  return "Minor"
-}
-};
+// const isAdult = (age) => {
+// if (age >= 18){
+//   return "Adult";
+// } else {
+//   return "Minor"
+// }
+// };
 
-console.log('Exercise 2 Result:', isAdult(18));
+// console.log('Exercise 2 Result:', isAdult(18));
+/////////////////////////////////////////////////
+/*
+Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
+
+const isCharAVowel = (c) => {
+
+  if (c==='a' || c==='e' || c==='i' || c==='o' || c==='u') {
+    //console.log("char :", c);
+    return true;
+  }
+  else {
+    return false;
+  }
+
+}
+
+
+console.log('Exercise 3 Result:', isCharAVowel("t"));
+///////////////////////////////////////////////////////
