52d3f852d107389dd90a70a07ee2479486cef658
George-25 <georgemallory26@gmail.com>
2024-08-28 15:41:52 -0400

    excercise-4

--- app.js
+++ app.js
@@ -51,18 +51,45 @@ Example: isCharAVowel('a') should return true.
 Complete the exercise in the space below:
 */
 
-const isCharAVowel = (c) => {
+// const isCharAVowel = (c) => {
 
-  if (c==='a' || c==='e' || c==='i' || c==='o' || c==='u') {
-    //console.log("char :", c);
-    return true;
-  }
-  else {
-    return false;
-  }
+//   if (c==='a' || c==='e' || c==='i' || c==='o' || c==='u') {
+//     //console.log("char :", c);
+//     return true;
+//   }
+//   else {
+//     return false;
+//   }
 
+// }
+
+// const isCharAVowel = (char) => {
+//   const vowels = ['a', 'e', 'i', 'o', 'u'];
+//   for (let i = 0; i < vowels.length; i++) {
+//     if (char === vowels[i]) {
+//       return true;
+//     } 
+    
+//   }
+//   return false;
+// };
+
+// console.log('Exercise 3 Result:', isCharAVowel("i"));
+///////////////////////////////////////////////////////
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+const generateEmail = (str1,str2) => {
+return `${str1}@${str2}`;
 }
 
-
-console.log('Exercise 3 Result:', isCharAVowel("t"));
-///////////////////////////////////////////////////////
+console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
+////////////////////////////////////////////////////////
