535d3433edab5feb6f37b389c042e9b0e0bdaf48
George-25 <georgemallory26@gmail.com>
2024-08-28 22:44:41 -0400

    exercise-9

--- app.js
+++ app.js
@@ -168,17 +168,49 @@ Example: convertTemperature(32, 'F') should return 0 (Celsius).
 
 Complete the exercise in the space below:
 */
-const convertTemperature = (temp,str) => {
- // let result;
-  if(str == 'C'){
-    return temp * (9/5) + 32;
-  } else if(str == 'F'){
-     return (temp - 32) * (5/9);
-  }
-  //return result;
-  return temp;
+// const convertTemperature = (temp,str) => {
+//  // let result;
+//   if(str == 'C'){
+//     return temp * (9/5) + 32;
+//   } else if(str == 'F'){
+//      return (temp - 32) * (5/9);
+//   }
+//   //return result;
+//   return temp;
+// }
+
+
+// console.log('Exercise 8 Result:', convertTemperature(32, "F"));
+//////////////////////////////////////////////////////////////
+/*
+Exercise 9: basicCalculator()
+
+Create a function named basicCalculator. 
+It should take three arguments: two numbers and a string representing 
+an operation ('add', 'subtract', 'multiply', 'divide'). 
+Perform the provided operation on the two numbers. 
+In operations where the order of numbers is important, 
+treat the first parameter as the first operand and the 
+second parameter as the second operand.
+
+Example: basicCalculator(10, 5, 'subtract') should return 5.
+
+Complete the exercise in the space below:
+*/
+const basicCalculator = (num1,num2,str) => {
+if(str === 'add'){
+  return num1 + num2;
+}else if(str === 'subtract'){
+  return num1 - num2;
+}else if (str === 'multiply'){
+  return num1 * num2;
+}else if (str === 'divide'){
+  return num1 / num2;
+} else {
+  return "Error";
+}
 }
 
 
-console.log('Exercise 8 Result:', convertTemperature(32, "F"));
-//////////////////////////////////////////////////////////////
+console.log('Exercise 9 Result:', basicCalculator(10, 5, "multiply"));
+///////////////////
